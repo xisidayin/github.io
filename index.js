e5a8fa42d294845904b48f5822bd6ebe2db59dea
xisidayin <69741190+xisidayin@users.noreply.github.com>
2020-08-19 14:34:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,395 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Get free food</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="free food">
-
-    <!-- Bootstrap core CSS -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Custom fonts for this template -->
-  <link href="fontawesome-free/css/all.min.css" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="index.css" rel="stylesheet">
-  <link href="animation.css" rel="stylesheet">
-</head>
-<body>
-  <section class="head">
-    <div class="header container">
-      <h1>Get Free Food . Online</h1>
-      <img class="header_pic" src="img/spices.jpg">
-    </div>
-    <div class="trans">
-
-    </div>
-  </section>
-
-  <section data-spy="scroll" data-target=".navdiv" class="item">
-    <div class="navdiv container">
-      <nav>
-        <ul>
-          <li>
-            <a href="#fooddelivery" class="js-scroll-trigger">Food Delivery</a>
-          </li>
-          <li>
-            <a href="#carshare" class="js-scroll-trigger">Car Share</a>
-          </li>
-          <li>
-            <a href="#driver" class="js-scroll-trigger">Driver</a>
-          </li>
-          <li>
-            <a href="#cashback" class="js-scroll-trigger">Cashback</a>
-          </li>
-          <li>
-            <a href="#grocery" class="js-scroll-trigger">Grocery</a>
-          </li>
-          <li>
-            <a href="#hotel" class="js-scroll-trigger">Hotel</a>
-          </li>
-          <li>
-            <a href="#bus" class="js-scroll-trigger">Bus</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-    <div id="fooddelivery">
-      <h2>Food Delivery</h2>
-      <div class="container">
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/grubhub.jpg" alt="Grubhub">
-          </div>
-          <div class="item_deets">
-            <h3>Grubhub</h3>
-            <p>Get $10 off for first order</p>
-          </div>
-          <div class="button">
-            <a class="button_1" href="https://www.grubhub.com/referral/b7d7b4e0-f914-11e9-b0d8-b9aaed579995" target="_blank">Get Deal</a>
-          </div>
-        </div>
-
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/UberEats.jpg" alt="UberEats">
-          </div>
-          <div class="item_deets">
-            <h3>Uber Eats</h3>
-            <p>Get $7 off for first order</p>
-          </div>
-          <div class="button">
-            <a class="button_2" href="#" href="http://ubr.to/EatsGiveGet" target="_blank">Code</a><a class="button_3" href="#" href="http://ubr.to/EatsGiveGet" target="_blank">eats-3csf1bkau</a>
-          </div>
-        </div>
-
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/doordash.png" alt="Doordash">
-          </div>
-          <div class="item_deets">
-            <h3>Doordash</h3>
-            <p>Get $15 off for first 3 order</p>
-          </div>
-          <div class="button">
-            <a class="button_1" href="https://drd.sh/iew1tH/"  target="_blank">Get Deal</a>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-    <div id="carshare">
-      <h2>Car Share</h2>
-      <div class="container">
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/lyft.jpg" alt="Lyft">
-          </div>
-          <div class="item_deets">
-            <h3>Lyft</h3>
-            <p>Get $5 off for first ride</p>
-          </div>
-          <div class="button">
-            <a class="button_1"  target="_blank" href="https://www.lyft.com/i/ERIC84970?utm_medium=p2pi_iacc">Get Deal</a>
-          </div>
-        </div>
-
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/uber.jpg" alt="Uber">
-          </div>
-          <div class="item_deets">
-            <h3>Uber</h3>
-            <p>Get $6 off for first 3 rides</p>
-          </div>
-          <div class="button">
-            <a class="button_2"  target="_blank" href="https://www.uber.com/invite/3csf1bkaue">Code</a><a class="button_3"  target="_blank" href="https://www.uber.com/invite/3csf1bkaue">eats-3csf1bkau</a>
-          </div>
-        </div>
-
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/freebird.jpg" alt="Freebird">
-          </div>
-          <div class="item_deets">
-            <h3>Freebird</h3>
-            <p>Get $10 when you take 2 rides on Uber or Lyft using Freebird.</p>
-          </div>
-          <div class="button">
-            <a class="button_2"  target="_blank" href="https://my.fbird.co/rPj7MWslW1">Code</a><a class="button_3"  target="_blank" href="https://my.fbird.co/rPj7MWslW1">v66ce</a>
-          </div>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-    <div id="driver">
-      <h2>Driver</h2>
-      <div class="container">
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/lyft%20driver.png" alt="Lyft">
-          </div>
-          <div class="item_deets">
-            <h3>Lyft</h3>
-            <p>Make $2,100 guaranteed using this link to drive a lyft in Washington DC, or other available cities.</p>
-          </div>
-          <div class="button">
-            <a class="button_1"  target="_blank" href="https://www.lyft.com/drivers/ERIC84970?utm_medium=p2di_iacc">Get Deal</a>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-    <div id="cashback">
-      <h2>Cashback</h2>
-      <div class="container">
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/Rakuten.jpg" alt="Rakuten">
-          </div>
-          <div class="item_deets">
-            <h3>Rakuten / Ebates</h3>
-            <p>Get a $10 bonus when sign up. Cash back on hundreds of stores.</p>
-          </div>
-          <div class="button">
-            <a class="button_1"  target="_blank" href="https://go.ebat.es/YUSUN62?eeid=44747">Get Deal</a>
-          </div>
-        </div>
-
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/BeFrugal.png" alt="BeFrugal">
-          </div>
-          <div class="item_deets">
-            <h3>BeFrugal</h3>
-            <p>Earn $10 when sign up and shop through BeFrugal.</p>
-          </div>
-          <div class="button">
-            <a class="button_1"  target="_blank" href="https://www.befrugal.com/rs/ORPSZCC/">Get Deal</a>
-          </div>
-        </div>
-
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/Giving%20Assistant.png" alt="Giving Assistant">
-          </div>
-          <div class="item_deets">
-            <h3>Giving Assistant</h3>
-            <p>Earn $5 when sign up and shop through Giving Assistant.</p>
-          </div>
-          <div class="button">
-            <a class="button_1"  target="_blank" href="https://givingassistant.org/?rid=GddzgL8Wzs">Get Deal</a>
-          </div>
-        </div>
-
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/topcashback.png" alt="Topcashback">
-          </div>
-          <div class="item_deets">
-            <h3>Topcashback</h3>
-            <p>Earn cash back on Amazon, Bestbuy, Macys's or more stores.</p>
-          </div>
-          <div class="button">
-            <a class="button_1"  target="_blank" href="http://www.topcashback.com/ref/cjdsgsy?source=iphonemobSMS">Get Deal</a>
-          </div>
-        </div>
-
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/ibotta.png" alt="Ibotta">
-          </div>
-          <div class="item_deets">
-            <h3>Ibotta</h3>
-            <p>Earn cash back on Uber, Walmart, Domino's or more stores.</p>
-          </div>
-          <div class="button">
-            <a class="button_2"  target="_blank"  href="https://ibotta.onelink.me/iUfE/1005cd3f">Code</a><a class="button_3"  target="_blank"  href="https://ibotta.onelink.me/iUfE/1005cd3f">avuermj</a>
-          </div>
-        </div>
-
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/drop.jpg" alt="Drop">
-          </div>
-          <div class="item_deets">
-            <h3>Drop</h3>
-            <p>Earn $5 dollars. the free app that's giving away millions in cash rewards.</p>
-          </div>
-          <div class="button">
-            <a class="button_2"  target="_blank"  href="https://b.ewd.io/code?c=v066j">Code</a><a class="button_3"  target="_blank"  href="https://b.ewd.io/code?c=v066j">v066j</a>
-          </div>
-        </div>
-
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/swagbucks.png" alt="Swagbucks">
-          </div>
-          <div class="item_deets">
-            <h3>Swagbucks</h3>
-            <p>Earn 150 SB points when signup for a new account.</p>
-          </div>
-          <div class="button">
-            <a class="button_1"  target="_blank"  href="https://www.swagbucks.com/?cmd=sb-register&rb=59217494&cmp=197&cxid=2001-app">Get Deal</a>
-          </div>
-        </div>
-
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/mypoints.png" alt="Mypoints">
-          </div>
-          <div class="item_deets">
-            <h3>Mypoints</h3>
-            <p>Earn $10 Amazon or VISA gift card when signup for a new account.</p>
-          </div>
-          <div class="button">
-            <a class="button_1"  target="_blank"  href="https://www.mypoints.com?rb=75758269">Get Deal</a>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-    <div id="grocery">
-      <h2>Grocery</h2>
-      <div class="container">
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/walmart.jpg" alt="Walmart">
-          </div>
-          <div class="item_deets">
-            <h3>Walmart</h3>
-            <p>Walmart does the shopping and loads your car for FREE. How easy is that? Here’s $10 to try it.</p>
-          </div>
-          <div class="button">
-            <a class="button_1"  target="_blank" href="http://r.wmt.co/v/get_free_food">Get Deal</a>
-          </div>
-        </div>
-
-        <div class="item_box">
-          <div class="item_pic">
-            <img src="img/711.jpg" alt="711">
-          </div>
-          <div class="item_deets">
-            <h3>7 Eleven</h3>
-            <p>Get 1000 points for you when you sign up.</p>
-          </div>
-          <div class="button">
-            <a class="button_2"  target="_blank" href="https://smart.link/qlhucqy5752su?site_id=04-28-2020&creative_id=rafloyalty&member_id=1758773300335830718&cp_1=yhgxsd">Code</a><a class="button_3"  target="_blank" href="https://smart.link/qlhucqy5752su?site_id=04-28-2020&creative_id=rafloyalty&member_id=1758773300335830718&cp_1=yhgxsd">yhgxsd</a>
-          </div>
-        </div>
-
-      </div>
-    </div>
+(function($) {
+  "use strict"; // Start of use strict
 
 
-        <div id="hotel">
-          <h2>Hotel</h2>
-          <div class="container">
-            <div class="item_box">
-              <div class="item_pic">
-                <img src="img/airbnb.jpg" alt="Airbnb">
-              </div>
-              <div class="item_deets">
-                <h3>Airbnb</h3>
-                <p>Get up to $35 off your first Airbnb trip.</p>
-              </div>
-              <div class="button">
-                <a class="button_1"  target="_blank" href="https://abnb.me/e/p6jNeqBKQ3">Get Deal</a>
-              </div>
-            </div>
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
 
-            <div class="item_box">
-              <div class="item_pic">
-                <img src="img/airbnbhost.png" alt="AirbnbHost">
-              </div>
-              <div class="item_deets">
-                <h3>Airbnb Host</h3>
-                <p>Rent out rooms and get more moeny.</p>
-              </div>
-              <div class="button">
-                <a class="button_1"  target="_blank" href="https://abnb.me/e/p6jNeqBKQ3">Get Deal</a>
-              </div>
-            </div>
+     // Callback after animation
 
-            <div class="item_box">
-              <div class="item_pic">
-                <img src="img/marriott.png" alt="Marriott">
-              </div>
-              <div class="item_deets">
-                <h3>Marriott</h3>
-                <p>Enjoy Member Rates and get free Wi-Fi. Plus, you’ll earn 2,000 bonus points for each of your first five paid stays.</p>
-              </div>
-              <div class="button">
-                <a class="button_1"  target="_blank" href="https://abnb.me/e/p6jNeqBKQ3">Get Deal</a>
-              </div>
-            </div>
+     // Must change focus!
 
-          </div>
-        </div>
+     var $target = $(target);
 
+     $target.focus();
 
-            <div id="bus">
-              <h2>Bus</h2>
-              <div class="container">
-                <div class="item_box">
-                  <div class="item_pic">
-                    <img src="img/ourbus.png" alt="Our Bus">
-                  </div>
-                  <div class="item_deets">
-                    <h3>Our Bus</h3>
-                    <p>Get $10 off first ride.</p>
-                  </div>
-                  <div class="button">
-                    <a class="button_1"  target="_blank" href="https://z2u3s.app.goo.gl/?link=https://www.ourbus.com/signup/OBHBI8MK?deeplink=ourbus://referfriend/OBHBI8MK&apn=com.ourbus.commuter&ibi=com.ourbus.ourbus&isi=1089117895">Get Deal</a>
-                  </div>
-                </div>
+     if ($target.is(":focus")) { // Checking if the target was focused
 
-              </div>
-            </div>
+      return false;
 
+     } else {
 
-      </div>
-    </div>
+      $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
 
-  </section>
+      $target.focus(); // Set focus again
 
-  <footer>
-    <P>Get Free Food . Online &nbsp; &nbsp; 2020</P>
-  </footer>
+     };
 
+    }
+        );
+        }
+      }
+    });
 
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="index.js"></script>
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navdiv').collapse('hide');
+  });
 
-</body>
-</html>
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '.navdiv',
+    offset: 300
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($(".navdiv").offset().top > 400) {
+      $(".navdiv").addClass("shadow");
+    } else{
+      $(".navdiv").removeClass("shadow");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  $('.button_3').hover(function() {
+    $('.button_2').toggleClass('button_4');
+  });
+
+})(jQuery); // End of use strict
